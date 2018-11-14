2912496bba5178ab3c0486241664fa345e5df5cf
HUYNH <cong-lap.huynh@etu.univ-lyon1.fr>
2018-11-14 10:00:32 +0100

    First commit

---
+++ fichier_nano/figure.h
@@ -0,0 +1,9 @@
+#ifndef FIGURE_H
+#define FIGURE_H
+#include "o_objet.h"
+#include "t_geometrie.h"
+
+double *calcul_cercle(double *centre, double rayon, int nb_points);
+Grille_triplet calcul_sphere(Triplet centre, double rayon, int nb_points);
+
+#endif
